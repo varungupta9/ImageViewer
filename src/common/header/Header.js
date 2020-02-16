ac7e59e17be3bd7adae88d160a72e0aa40b50bc9
varungupta9 <varun4real@gmail.com>
2020-02-17 01:44:52 +0530

    Common header with CSS styling

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -8,98 +8,89 @@ import Avatar from '@material-ui/core/Avatar';
 import MenuItem from '@material-ui/core/MenuItem';
 import { Popover } from '@material-ui/core';
 
-
-
+//Common header for all screens.
 class Header extends Component
 {
-    constructor()
-    {
+    constructor() {
         super();
-        this.state={
+        this.state = {
             anchorEl: null,
-            loggedIn: sessionStorage.getItem('access-token')!= null
+            loggedIn: sessionStorage.getItem('access-token') != null //Retrieve access token stored in session
         }
     }
-
     onProfileIconHandler=(event) =>
     {
         this.setState({
             anchorEl: event.currentTarget
           })
+    }  
+    handleClose = () => {
+        this.setState({ anchorEl: null }); //Hides the modal 
     }
 
-    
-  handleClose = () =>{
-    this.setState({ anchorEl: null });
-  }
+    myAccountClickHanlder = () => { //Function callled when image icon is clicked
+        this.props.history.push("/profile"); //Route to Profile page 
+    }
 
-  myAccountClickHanlder = () =>
-  {
-    this.props.history.push("/profile");
-  }
+    logoutClickHandler = () => { //Function callled Logout is clicked
+        sessionStorage.removeItem('access-token'); //removes the access-token from session
+        this.setState({ loggedIn: false }); 
+    }
 
-  logoutClickHandler = () =>
-  {
-      sessionStorage.removeItem('access-token');
-      this.setState({loggedIn:false});
-  }
+    searchItemHanlder = (event) => {
+        this.props.searchItemHanlder(event.target.value);
+    };
 
-
-  searchItemHanlder = (event) => {
-    this.props.searchItemHanlder(event.target.value);
-  };
-
-
+    //Renders header on all pages with specific components for individual screen.
     render()
-
     {
-        return(
-        <div >
-            <header className="app-header">
-            {!this.state.loggedIn && <Redirect to='/'/>}
-            <div className="logo">
-            <Link to="/home" className="logo-style">Image Viewer</Link>
+        return (
+            <div >
+                <header className="app-header">
+                    {!this.state.loggedIn && <Redirect to='/' />}
+                    <div className="logo">
+                        <Link to="/home" className="logo-style">Image Viewer</Link>
+                    </div>
+                    {this.state.loggedIn &&
+                        <div className="right">
+                            {this.props.pageId === 'home' &&
+                                <div className="searchBox">
+                                    <SearchOutlinedIcon />
+                                    <Input className="serach-area" placeholder="Search..." disableUnderline onChange={this.searchItemHanlder} />
+                                </div>
+                            }
+                            <IconButton className="icons" onClick={this.onProfileIconHandler}>
+                                <Avatar src={this.props.profilePicture} alt="PF" ></Avatar>
+                                <Popover
+                                    id="simple-menu"
+                                    anchorEl={this.state.anchorEl}
+                                    open={Boolean(this.state.anchorEl)}
+                                    onClose={this.handleClose}
+                                    anchorOrigin={{
+                                        vertical: 'bottom',
+                                        horizontal: 'left',
+                                    }}
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'left',
+                                    }}
+                                >
+                                    <div className="popmenu">
+                                        {this.props.pageId === 'home' &&
+                                            <div>
+                                                <MenuItem onClick={this.myAccountClickHanlder}>My Account</MenuItem>
+                                                <hr className="line-separator-one" />
+                                            </div>}
+                                        <div>
+                                            <MenuItem onClick={this.logoutClickHandler}>Logout</MenuItem>
+                                        </div>
+                                    </div>
+                                </Popover>
+                            </IconButton>
+                        </div>
+                    }
+                </header>
             </div>
-            {this.state.loggedIn &&
-            <div className="right">
-            {this.props.pageId ==='home' &&    
-            <div className = "searchBox">
-                <SearchOutlinedIcon/>
-           <Input className="serach-area" placeholder = "Search..." disableUnderline onChange={this.searchItemHanlder}/> 
-           </div>
-            }
-           <IconButton className="icons" onClick={this.onProfileIconHandler}>
-               <Avatar src={this.props.profilePicture} alt="PF" ></Avatar>
-               <Popover
-                   id="simple-menu"
-                  anchorEl={this.state.anchorEl}
-                  open={Boolean(this.state.anchorEl)}
-                 onClose={this.handleClose}
-                 anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                  }}
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
-                  }}
-                >
-                    <div className="popmenu">
-                    {this.props.pageId ==='home' &&  
-                    <div>     
-        <MenuItem onClick={this.myAccountClickHanlder}>My Account</MenuItem>
-                <hr className="line-separator-one"/> 
-                </div> }
-                    <div>
-        <MenuItem onClick={this.logoutClickHandler}>Logout</MenuItem>
-        </div>
-        </div>
-      </Popover>
-           </IconButton>
-            </div>
-            }
-            </header> 
-        </div> 
         );
     }
 
